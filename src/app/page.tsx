35e4d3a7c5afe40080f821637dbdb5c2bb1560b4
Alessandro Reis <alessandrodreis23@gmail.com>
2025-09-08 13:15:19 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import { Header } from "@/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import { Header } from "@/components/common/header";
+import { ProductList } from "@/components/common/product-list";
+import { db } from "@/db";
+
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variant: true,
+    },
+  });
+
   return (
     <>
       <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Seja automático"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 }
