ef938bff3b4e7a126b0934fa8ca91865e4294918
Alessandro Reis <alessandrodreis23@gmail.com>
2025-09-16 09:17:42 -0300

    feat: add order tables

--- src/app/cart/identification/components/addresses.tsx
+++ src/app/cart/identification/components/addresses.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { PatternFormat } from "react-number-format";
@@ -28,6 +29,8 @@ import { useCreateShippingAddress } from "@/hooks/mutations/use-create-shipping-
 import { useUpdateCartShippingAddress } from "@/hooks/mutations/use-update-cart-shipping-address";
 import { useUserAddresses } from "@/hooks/queries/use-user-addresses";
 
+import { formatAddress } from "../../helpers/address";
+
 interface AddressesProps {
   shippingAddresses: (typeof shippingAddressTable.$inferSelect)[];
   defaultShippingAddressId: string | null;
@@ -37,6 +40,7 @@ const Addresses = ({
   shippingAddresses,
   defaultShippingAddressId,
 }: AddressesProps) => {
+  const router = useRouter();
   const [selectedAddress, setSelectedAddress] = useState<string | null>(
     defaultShippingAddressId || null,
   );
@@ -89,6 +93,7 @@ const Addresses = ({
         shippingAddressId: selectedAddress,
       });
       toast.success("Endereço vinculado para entrega!");
+      router.push("/cart/confirmation");
     } catch (error) {
       toast.error("Erro ao selecionar endereço. Tente novamente!");
       console.error(error);
@@ -110,14 +115,7 @@ const Addresses = ({
                   <div className="flex-1">
                     <Label htmlFor={address.id} className="cursor-pointer">
                       <div>
-                        <p className="text-sm">
-                          {address.recipientName} * {address.street},{" "}
-                          {address.number}
-                          {address.complement &&
-                            `, ${address.complement}`}, {address.neighborhood},
-                          {address.city} - {address.state} * CEP:{" "}
-                          {address.zipCode}
-                        </p>
+                        <p className="text-sm">{formatAddress(address)}</p>
                       </div>
                     </Label>
                   </div>
